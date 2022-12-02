05d4534e1333674456eb4586fc1da47b98a04262
Nate Ensign <dixienate@gmail.com>
2022-12-02 11:30:40 -0700

    movie watch list app completed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
-import React, {useEffect, useState} from 'react'
-import './App.css';
-import Header from './components/Header'
+import React, { useState, useEffect } from "react"
+import Header from "./components/Header"
+import "./App.css"
 import axios from "axios"
-import MovieScreen from './components/MovieScreen'
 
-
-
+import Watchlist from "./components/Watchlist"
+import MovieScreen from "./components/MovieScreen"
 
 function App() {
   const [list, setList] = useState([])
   const [movieList, setMovieList] = useState([])
   const [page, setPage] = useState(1)
 
+  const addMovie = (movie) => setList([...list, movie])
+
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie
+    });
+    setList(newState)
+  };
+
   const getData = () => {
     axios
-        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
-        .then((res) => {
-            console.log(res.data.results);
-            setMovieList(res.data.results);
-        });
-};
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res.data.results)
+        setMovieList(res.data.results)
+      });
+  };
 
-useEffect(() => {
-    getData();
-}, [page]);
+  useEffect(() => {
+    getData()
+  }, [page])
 
   return (
     <div className="App">
       <Header />
       <main>
         <MovieScreen
-        list={list}
-        page={page}
-        setPage={setPage}
-        movieList={movieList}
-         />
+          addMovie={addMovie}
+          page={page}
+          movieList={movieList}
+          list={list}
+          setPage={setPage}
+          removeMovie={removeMovie}
+        />
+        <Watchlist list={list} removeMovie={removeMovie}/>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
